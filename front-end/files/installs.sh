310da406b48ae7e5dd03b0b4034f858dc7d169d5
tidika <tochi.idika@gmail.com>
2024-07-31 18:02:02 -0500

    updated files.

---
+++ front-end/files/installs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e 
+exec > /tmp/user-data.log 2>&1
+echo "Updating packages and installing necessary dependencies..."
+sudo yum update -y
+sudo yum install -y ruby wget
+echo "Installing NVM..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+. ~/.nvm/nvm.sh
+echo "Installing Node.js..."
+nvm install 12
+nvm use 12
+nvm alias default 12
+sudo ln -s $(which node) /usr/bin/
+sudo ln -s $(which npm) /usr/bin/
+echo "Downloading files from S3..."
+cd /home/ec2-user
+sudo aws s3 cp s3://tochukwu-idika-s3-bucket/installs.sh /home/ec2-user --recursive
+echo "Starting Node.js application..."
+nohup node front-end-server.js > outputs.log 2>&1 &
+echo "UserData script complete."
--- front-end/files/start.sh
+++
@@ -1,3 +0,0 @@
-
-cd /home/ec2-user/files
-node front-end-server.js
